2801e76b2bae8cac8226f614c309dfde06ebe5d5
Marcus Lee <ml2182@cam.ac.uk>
2025-12-14 18:47:43 +0000

    Rename project and update source files

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -1,10 +1,9 @@
 # create_project.tcl
 
-create_project DivideBy4 ./DivideBy4 -part xc7a35tcpg236-1 -force
+create_project DivideBy4-Improved ./DivideBy4-Improved -part xc7a35tcpg236-1 -force
 
 # Add your HDL source files
-add_files ./src/IntegratedDesign.sv
-add_files ./src/JKBistable.sv
-add_files ./src/tb_Divide_By_Four.sv
-set_property top tb_Divide_By_Four [get_filesets sim_1]
+add_files ./src/FPGA_Design.sv
+add_files ./src/tb_DivideBy4.sv
+set_property top tb_DivideBy4 [get_filesets sim_1]
 launch_simulation
